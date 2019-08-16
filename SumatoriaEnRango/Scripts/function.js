eb278f807f6fec45fca73efc73a7c3adef44056b
Stovar97 <halito2008@gmail.com>
2019-08-16 15:33:19 -0500

    first

---
+++ SumatoriaEnRango/Scripts/function.js
@@ -0,0 +1,9 @@
+﻿function sumatoria() {
+    var i = document.getElementById('txtRangoInferior').value;
+    var j = document.getElementById('txtRangoSuperior').value;
+    var res = 0;
+    for (x = i; x <= j; x++) {
+        res = parseInt(res) + parseInt(x);
+    }
+    alert(res);
+}
